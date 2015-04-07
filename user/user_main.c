4b6b7168c2f7ec10738275fd7a18ef86d466cb93
unfoundbug <iam@theadmin.co.uk>
2015-04-07 12:56:20 +0100

    Migrating data to the header

--- user/user_main.c
+++ user/user_main.c
@@ -1,12 +1,5 @@
-#include "ets_sys.h"
-#include "osapi.h"
-#include "gpio.h"
-#include "os_type.h"
 #include "user_config.h"
-#include "user_interface.h"
-#include "mem.h"
-#include "espconn.h"
-
+#include "NonVol.h"
 //Tasks
 #define user_procTaskPrio        0
 #define user_procTaskQueueLen    2
@@ -31,37 +24,6 @@ uint8 pBuffer[128]; //Temporary buffer for structures
 //Timers
 os_timer_t tConnectionTimer;
 os_timer_t tStatusTimer;
-
-#define scheduleCall(Function, sig, par) { system_os_task(Function, 1, user_procTaskQueue, user_procTaskQueueLen); system_os_post(1, sig, par); }
-
-//Function definitions
-
-void user_init();
-
-static void checkConnection();
-static void setupLocalAP();
-static void connectToRemoteAP();
-
-static void enableTCPServer(uint32 iTCPPort, struct espconn* pConnection);
-static void disableTCPServer(uint32 iTCPPort, struct espconn* pConnection);
-
-static void handleConnectionEstablished(void* pArg);
-static void handleRecievedData(void* arg, char* pData, unsigned short iDataLen);
-static void handleConnectionDropped(void* pArg);
-
-static void processCommand(struct espconn* pTarget, char* pData, uint16 iLength);
-static void processTransfer(struct espconn* pTarget, char* pData, uint16 iLength);
-
-static void UpdateLocalAPDetails();
-static void UpdateRemoteAPDetails();
-static void Reboot();
-
-static void loop(os_event_t *events);
-
-#define pinHigh() GPIO_OUTPUT_SET(2,1); os_delay_us(29);
-#define pinLow() GPIO_OUTPUT_SET(2,0); os_delay_us(29);
-
-#define midiBit(bByte, oSet) if(bByte&oSet) {pinHigh();} else{ pinLow();}
 
 void sendMidiByte(char bByte) {
 	//StartBit
